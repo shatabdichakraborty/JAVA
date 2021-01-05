78013f25ded1ae63bb054dcde00722d62255c8e0
Shatabdi Chakraborty <51456489+shatabdichakraborty@users.noreply.github.com>
2021-01-05 23:53:44 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,40 @@
+package strings;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		
+		String a = "aaabc";
+		String b = "abccc";
+		boolean isAnagram = false;
+		
+		boolean visited[] = new boolean[b.length()];
+		
+			if (a.length() ==  b.length()) {
+			for (int i=0; i<a.length(); i++) {
+				
+				char c = a.charAt(i);
+				isAnagram = false;
+				
+				for(int j=0; j<b.length(); j++) {
+					
+					if (b.charAt(j) == c && !visited[j]){
+						visited[j] = true; 
+						isAnagram = true;
+						break;
+					}
+				}
+				if(!isAnagram) {
+					break;
+				}
+			}
+			}
+		
+		if(isAnagram) {
+			System.out.println("this is anagram");
+		}else {
+			System.out.println("not an anagram");
+		}
+	}
+
+}
