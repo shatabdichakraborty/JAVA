5c55f536f2c749c280f9d2e159dd27cbe619cb34
Shatabdi Chakraborty <51456489+shatabdichakraborty@users.noreply.github.com>
2021-02-03 23:49:42 +0530

    Add files via upload

---
+++ loops/ForLoops.java
@@ -0,0 +1,41 @@
+package loops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+	
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("hello world" +i);
+		
+//		for(int i=0;i<=10;i=i+2)
+//		{
+//			System.out.println(i);
+//			
+//		}
+		
+//		int sum=0;
+//		int n=5;
+//		for (int i=1;i<=n;i++)
+//		{
+//			sum=sum+i;     
+//		}
+//			System.out.println(sum);
+		
+//		int n=2;
+//		int result=0;
+//		for(int i=1;i<=12;i++) {
+//			result=n*i;
+//			System.out.println(""+result);
+//		}
+		
+		int n=10;
+		int fact=1;
+		for (int i=1;i<=n;i++) {
+			fact=fact*i;
+	
+		}
+			System.out.println(fact);
+		}
+		}
+	
+
---
+++ loops/Pattern3.java
@@ -0,0 +1,25 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n");
+		int n=sc.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			for(int j=1;j<2*(n-i);j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
+
